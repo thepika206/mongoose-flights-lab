bf62bd7c4849763ee5bef406e603876ec3b3abab
pcheung <pika206@gmail.com>
2022-09-23 15:42:04 -0500

    setup db connection, nodemon running okay

---
+++ models/flight.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+	
+const flightSchema = new Schema({
+  airline: String,
+  airport: String,
+  flightNo: Number,
+  departs: Date,
+},{
+  timestamps: true
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
