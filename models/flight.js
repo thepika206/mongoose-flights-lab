08b423030dd5a6580489e9ae65f8679e923d635e
pcheung <pika206@gmail.com>
2022-09-26 16:53:08 -0500

    impl show flight view that includes the existing tickets, and a form to add new ticket

--- models/flight.js
+++ models/flight.js
@@ -3,7 +3,16 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema
 
 const ticketSchema = new Schema({
-
+  seat: {
+    type: String,
+    match: /[A-F][1-9]\d?/
+  },
+  price: {
+    type: Number,
+    min: 0
+  }
+},{
+  timestamps: true
 })
 
 	
@@ -27,6 +36,7 @@ const flightSchema = new Schema({
     type: Date,
     default: dateFromNow(365)
   },
+  tickets: [ticketSchema]
 },{
   timestamps: true
 })
