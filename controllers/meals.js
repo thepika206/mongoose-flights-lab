d4fd7c4eadd13645d7a5ba1577e9acadec579659
pcheung <pika206@gmail.com>
2022-09-28 08:04:54 -0500

    add unique property on meals name ,remove logging the meals array

--- controllers/meals.js
+++ controllers/meals.js
@@ -8,7 +8,6 @@ import { Meal } from '../models/meal.js'
 function newMeal(req,res){
   Meal.find({})
   .then(meals => {
-    console.log(meals, 'meals array')
     res.render('meals/new', {
       meals: meals,
       title: 'Add Meal'
@@ -17,13 +16,14 @@ function newMeal(req,res){
 }
 
 function create(req,res){
-  console.log(req.body)
+  // console.log(req.body)
   Meal.create(req.body)
   .then(meal => {
     res.redirect('/meals/new')
   })
   .catch(err => {
-    res.redirect('/flights')
+    console.log(err)
+    res.redirect('/meals/new')
   })
 }
 
--- models/meal.js
+++ models/meal.js
@@ -6,6 +6,7 @@ const mealSchema = new Schema({
   name: {
     type: String,
     required: true,
+    unique: true,
   }
 },{
   timestamps: true
